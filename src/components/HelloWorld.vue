456df91f814c48d9162154f9d8c1cb743f3c6e19
milyyy <milysong@163.com>
2019-09-02 15:33:34 +0800

    prop传值

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1> -->
+    <section class="section">
+      <h3>{{ msg.title }}</h3>
+      <h6>{{ msg.content }}</h6>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.section
+  background #f1f1f1
+  margin-bottom 10px
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <!-- 这里直接穿了一个msg对象给子组件 -->
+    <HelloWorld v-for="post in posts" :key="post.id" :msg="post" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue", content: "content01" },
+        { id: 2, title: "Blogging with Vue", content: "content02" },
+        { id: 3, title: "Why Vue is so fun", content: "content03" }
+      ]
+    };
+  }
+};
+</script>
