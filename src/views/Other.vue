01e0e560db0c2844c35bf9071dcf937f81c47ee7
milyyy <milysong@163.com>
2019-11-14 16:54:04 +0800

    .

--- src/views/Other.vue
+++ src/views/Other.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="other">
     <p>导航</p>
-    <router-link to="/other/route1">route1</router-link>&nbsp;&nbsp;
-    <router-link to="/other/route2">route2</router-link>
+    <router-link to="/other/first">route1</router-link>&nbsp;&nbsp;
+    <router-link to="/other/second">route2</router-link>
     <router-view></router-view>
   </div>
 </template>
