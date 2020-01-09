1a1775968a78af76a586465d6bdf54cc0043d87b
milyyy <milysong@163.com>
2020-01-09 17:19:53 +0800

    getters

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,13 @@ export default new Vuex.Store({
     num: 1,
 
     todos: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false }
+      { id: 1, text: 'text01', done: true },
+      { id: 2, text: 'text02', done: false }
     ]
   },
 
-  // Getter 可以认为是store的计算属性
+  // Getter 可以认为是store的计算属性(依赖state)
+  // 1.在通过属性访问时是作为 Vue 的响应式系统的一部分缓存其中的
   getters: {
     // 接受 state 作为其第一个参数：
     doneTodos: state => {
@@ -24,6 +25,10 @@ export default new Vuex.Store({
     // 也可以接受其他 getter 作为第二个参数:
     doneTodosCount: (state, getters) => {
       return getters.doneTodos.length
+    },
+    // 也可以通过让 getter 返回一个函数，来实现给 getter 传参
+    getTodoById: (state) => (id) => {
+      return state.todos.filter(todo => todo.id === id)
     }
   }
 })
--- src/views/Getter.vue
+++ src/views/Getter.vue
@@ -1,10 +1,60 @@
 <!--
  * @Date: 2020-01-09 14:49:45
- * @LastEditTime : 2020-01-09 14:50:04
+ * @LastEditTime : 2020-01-09 17:17:45
  * @FilePath: \_vue\src\views\Getters.vue
  -->
 <template>
   <div>
     <h3>This is Getters</h3>
+    
+    <div>
+      访问getter中的属性：
+      <div>
+        <!-- 单个getter属性获取 -->
+        <!-- <ul>
+          <li v-for="item in newArr" :key="item.id">
+            {{ item.text }}
+          </li>
+        </ul> -->
+
+        <!-- 多个getter属性获取 -->
+        <ul>
+          <li v-for="item in doneTodos" :key="item.id">
+            {{ item.text }}
+          </li>
+        </ul>
+        <br>
+        {{ $store.getters.doneTodosCount }} <br>
+
+      </div>
+      
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  // 在组件中使用getters
+  /*
+  * 单个属性获取
+    computed: {
+      newArr() {
+        // 这里doneTodos在getters中已经过滤为一个新的数组 
+        return this.$store.getters.doneTodos
+      }
+    },
+  */
+  //  mapGetters 辅助函数,多个属性获取
+  computed: {
+    ...mapGetters([
+      'doneTodos', 'doneTodosCount'
+    ]),
+    // mapGetters({
+    //   // 把 `this.doneCount` 映射为 `this.$store.getters.doneTodosCount`
+    //   doneCount: 'doneTodosCount'
+    // })
+  }
+
+}
+</script>
